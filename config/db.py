facfed702a9f26b8ff8bb9516dd1582796f37092
yonnycuesta <yocumo1998@gmail.com>
2024-12-17 23:38:54 -0500

    newdb

--- config/db.py
+++ config/db.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:snow123@localhost/api_etbkpi"
-
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:snow123@localhost/api_etbkpi"
 # Servidor LINUX
 # SQLALCHEMY_DATABASE_URL = "postgresql://admeiasa:eiasa.adm2024@db:5432/overtime_db"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://kpis_api_user:Yl95krRVDEoh45s93uxEfWAydOObVrk8@dpg-cth51t9opnds73auvsi0-a.oregon-postgres.render.com/kpis_api"
+
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
--- main.py
+++ main.py
@@ -52,7 +52,7 @@ app.include_router(kpi)
 
 
 
-# if __name__ == "__main__":
-#     import uvicorn
+if __name__ == "__main__":
+    import uvicorn
 
-#     uvicorn.run("app2:app", host="0.0.0.0", port=8002, reload=True)
+    uvicorn.run("app2:app", host="0.0.0.0", port=8002, reload=True)

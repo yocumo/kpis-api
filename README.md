5e4f1a438732482316cb5547664399ccbaf4fec1
yonnycuesta <yocumo1998@gmail.com>
2024-12-25 23:31:19 -0500

    mmm

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 #RUN SERVER
 uvicorn main:app --reload
+
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:snow123@localhost/api_etbkpi"
+# SQLALCHEMY_DATABASE_URL = "postgresql://api_etbkpi_owner:IzVKRov2qn6c@ep-lively-resonance-a52ts2qe.us-east-2.aws.neon.tech/api_etbkpi?sslmode=require"
+
+
+# Servidor LINUX
+SQLALCHEMY_DATABASE_URL = "postgresql://admeiasa:eiasa.adm2024@db:5432/etbkpi_manager"
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://kpis_api_user:Yl95krRVDEoh45s93uxEfWAydOObVrk8@dpg-cth51t9opnds73auvsi0-a.oregon-postgres.render.com/kpis_api"
+
--- config/db.py
+++ config/db.py
@@ -2,15 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:snow123@localhost/api_etbkpi"
-# SQLALCHEMY_DATABASE_URL = "postgresql://api_etbkpi_owner:IzVKRov2qn6c@ep-lively-resonance-a52ts2qe.us-east-2.aws.neon.tech/api_etbkpi?sslmode=require"
-
-
 # Servidor LINUX
 SQLALCHEMY_DATABASE_URL = "postgresql://admeiasa:eiasa.adm2024@db:5432/etbkpi_manager"
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://kpis_api_user:Yl95krRVDEoh45s93uxEfWAydOObVrk8@dpg-cth51t9opnds73auvsi0-a.oregon-postgres.render.com/kpis_api"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)

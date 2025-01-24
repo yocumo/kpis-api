44ee7e764e880ee05dda6c3d3ea43cb0148a65f7
yonnycuesta <yocumo1998@gmail.com>
2025-01-24 14:26:10 -0500

    PasswordChange

--- config/db.py
+++ config/db.py
@@ -3,10 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:snow123@localhost/api_etbkpi"
+
 # Servidor LINUX
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://admeiasa:eiasa.adm2024@68.178.204.192:5432/etbkpi_manager"
-)
+SQLALCHEMY_DATABASE_URL = ("postgresql://admeiasa:eiasa.adm2024@68.178.204.192:5432/etbkpi_manager")
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://api_etbkpi_owner:IzVKRov2qn6c@ep-lively-resonance-a52ts2qe.us-east-2.aws.neon.tech/api_etbkpi?sslmode=require"
 
--- main.py
+++ main.py
@@ -62,4 +62,4 @@ app.include_router(task_home)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
